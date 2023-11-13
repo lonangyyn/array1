af44cb507c2535e78e52d7b4d8303bb58f89790e
lonangyyn <150718542+lonangyyn@users.noreply.github.com>
2023-11-14 00:38:38 +0700

    Array
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+#define max 1000
+void nhapMang(int a[], int &n);
+void tinhTar(int a[], int n,int x);
+int main()
+{
+	int target,e;
+	cout << "target?" << endl;
+	cin >> target;
+	int arr[max];
+	cout << "so ptu?" << endl;
+	cin >> e;
+	nhapMang(arr, e);
+	tinhTar(arr, target,e);
+}
+void nhapMang(int a[], int& n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << "a[" << i << "]" << endl;
+		cin >> a[i];
+	}
+}
+void tinhTar(int a[], int n,int x)
+{
+	for (int i = 0; i < x-1; i++)
+	{
+		for (int m = 1; m < x; m++)
+		{
+			if ((a[i] + a[i + m]) == n)
+				cout << "[" << i << "," << i + m << "]" << endl;
+		}
+	}
+}
